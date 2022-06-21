3ae63a8c01cc5b4853a2e7e03bb492f2fe6badc0
Anastasiia-uk <107074764+Anastasiia-uk@users.noreply.github.com>
2022-06-22 00:16:36 +0300

    Add files via upload

---
+++ lab1/lab1/CalcArrInThread.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.ComponentModel;
+
+namespace lab1
+{
+    public class CalcArrInThread
+    {
+        public delegate void PercentProgress(int percent); 
+        public double[] Arr { get; private set; }
+        public Thread thread { get; private set; }
+        public PercentProgress PercentProgressHandler { get; set; }
+        //public int ProgressPercent { get; private set; } = 0;
+        private MainWindow.keys Key;
+        public CalcArrInThread(double[] Arr, MainWindow.keys Key)
+        {
+            this.Arr = Arr;
+            this.Key = Key;
+        }
+        public void StartThread()
+        {
+            switch (Key)
+            {
+                case MainWindow.keys.Sum:
+                    thread = new Thread(Sum);
+                    thread.Start();
+                    break;
+
+                case MainWindow.keys.Min:
+                    thread = new Thread(Min);
+                    thread.Start();
+                    break;
+                case MainWindow.keys.Max:
+                    thread = new Thread(Max);
+                    thread.Start();
+                    break;
+                case MainWindow.keys.SortDesc:
+                    thread = new Thread(SortDesc);
+                    thread.Start();
+                    break;
+                case MainWindow.keys.SortIncr:
+                    thread = new Thread(SortIncr);
+                    thread.Start();
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void StopThread()
+        {
+            
+            thread.Abort();
+        }
+        public void Sum()
+        {
+            double sum = 0;
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                PercentProgressHandler(i + 1);
+                sum += Arr[i];
+            }
+        }
+        void Min()
+        {
+            double min = Arr[0];
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                PercentProgressHandler(i + 1);
+                if (Arr[i] < min)
+                    min = Arr[i];
+            }
+        }
+        void Max()
+        {
+            double max = Arr[0];
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                PercentProgressHandler(i + 1);
+                if (Arr[i] > max)
+                    max = Arr[i];
+            }
+        }
+        void SortDesc() 
+        {
+            double buf = 0;
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                PercentProgressHandler(i + 1);
+                for (int j = 0; j < Arr.Length-1; j++)
+                {
+                    if(Arr[j] < Arr[j+1])
+                    {
+                        buf = Arr[j + 1];
+                        Arr[j + 1] = Arr[j];
+                        Arr[j] = buf;
+                    }
+                }
+            }
+        }
+        void SortIncr()
+        {
+            double buf = 0;
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                PercentProgressHandler(i + 1);
+                for (int j = 0; j < Arr.Length - 1; j++)
+                {
+                    if (Arr[j] > Arr[j + 1])
+                    {
+                        buf = Arr[j + 1];
+                        Arr[j + 1] = Arr[j];
+                        Arr[j] = buf;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ lab1/lab1/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace lab1
+{
+    public partial class MainWindow : Window
+    {
+        CalcArrInThread calcArrInThread1;
+        CalcArrInThread calcArrInThread2;
+        double[] arr;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Exit1Button.IsEnabled = false;
+            Exit2Button.IsEnabled = false;
+        }
+        public enum keys
+        {
+            Sum,
+            Min,
+            Max,
+            SortDesc,
+            SortIncr
+        };
+        void InitArr(int size)
+        {
+            arr = new double[size];
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(-1000, 1000);
+            }
+        }
+        void PercentToPgBar1(int percent)
+        {
+            Dispatcher.Invoke(delegate () {
+                PbTh1.Value = (Convert.ToDouble(percent) / Convert.ToDouble(arr.Length)) * 100;
+                tb1.Text = $"{PbTh1.Value}%/100%";
+            });
+        }
+        void PercentToPgBar2(int percent)
+        {
+            Dispatcher.Invoke(delegate () {
+                PbTh2.Value = (Convert.ToDouble(percent) / Convert.ToDouble(arr.Length)) * 100;
+                tb2.Text = $"{PbTh2.Value}%/100%";
+            });
+        }
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            InitArr(100000);
+            calcArrInThread1 = new CalcArrInThread(arr, keys.Max);
+            calcArrInThread1.PercentProgressHandler = PercentToPgBar1;
+
+            InitArr(10000);
+            calcArrInThread2 = new CalcArrInThread(arr, keys.SortDesc);
+            calcArrInThread2.PercentProgressHandler = PercentToPgBar2;
+
+            calcArrInThread1.StartThread();
+            calcArrInThread2.StartThread();
+            Exit1Button.IsEnabled = Exit2Button.IsEnabled = true;
+        }
+
+        private void Exit1Button_Click(object sender, RoutedEventArgs e)
+        {
+            calcArrInThread1.StopThread();
+            if (PbTh1.Value < 100)
+                MessageBox.Show($"Thread canceled at {PbTh1.Value.ToString()}%");
+            PbTh1.Value = 0;
+            tb1.Text = "";
+            Exit1Button.IsEnabled = false;
+        }
+        private void Exit2Button_Click(object sender, RoutedEventArgs e)
+        {
+            calcArrInThread2.StopThread();
+            if (PbTh2.Value < 100)
+                MessageBox.Show($"Thread canceled at {PbTh2.Value.ToString()}%");
+            PbTh2.Value = 0;
+            tb2.Text = "";
+            Exit2Button.IsEnabled = false;
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (Exit1Button.IsEnabled)
+                calcArrInThread1.StopThread();
+            if (Exit2Button.IsEnabled)
+                calcArrInThread2.StopThread();
+        }
+    }
+}
