7f65213bcec586f7a0245e272724e614010ba9c5
Anastasiia-uk <107074764+Anastasiia-uk@users.noreply.github.com>
2022-06-22 00:21:53 +0300

    Add files via upload

---
+++ lab2/lab2/CalcArrAsync.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace lab2
+{
+    public class CalcArrAsync
+    {
+        public delegate void PercentProgress(int percent);
+        public PercentProgress PercentProgressHandler { get; set; }
+        public double[] Arr { get; private set; }
+        private MainWindow.keys Key;
+
+        public CalcArrAsync(double[] Arr, MainWindow.keys Key)
+        {
+            this.Arr = Arr;
+            this.Key = Key;
+        }
+        public void StartAsync()
+        {
+            switch (Key)
+            {
+                case MainWindow.keys.Sum:
+                    Sum();
+                    break;
+                case MainWindow.keys.Min:
+                    Min();
+                    break;
+                case MainWindow.keys.Max:
+                    Max();
+                    break;
+                case MainWindow.keys.SortDesc:
+                    SortDesc();
+                    break;
+                case MainWindow.keys.SortIncr:
+                    SortIncr();
+                    break;
+                default:
+                    break;
+            }
+        }
+        async void Sum()
+        {
+            double sum = 0;
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    PercentProgressHandler(i + 1);
+                    sum += Arr[i];
+                }
+            });
+        }
+        async void Min()
+        {
+            double min = Arr[0];
+            await Task.Run(() => 
+            {
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    PercentProgressHandler(i + 1);
+                    if (Arr[i] < min)
+                        min = Arr[i];
+                }
+            });
+        }
+        async void Max()
+        {
+            double max = Arr[0];
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    PercentProgressHandler(i + 1);
+                    if (Arr[i] > max)
+                        max = Arr[i];
+                }
+            });
+        }
+        async void SortDesc()
+        {
+            double buf = 0;
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    PercentProgressHandler(i + 1);
+                    for (int j = 0; j < Arr.Length - 1; j++)
+                    {
+                        if (Arr[j] < Arr[j + 1])
+                        {
+                            buf = Arr[j + 1];
+                            Arr[j + 1] = Arr[j];
+                            Arr[j] = buf;
+                        }
+                    }
+                }
+            });
+        }
+        async void SortIncr()
+        {
+            double buf = 0;
+            await Task.Run(() => 
+            {
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    PercentProgressHandler(i + 1);
+                    for (int j = 0; j < Arr.Length - 1; j++)
+                    {
+                        if (Arr[j] > Arr[j + 1])
+                        {
+                            buf = Arr[j + 1];
+                            Arr[j + 1] = Arr[j];
+                            Arr[j] = buf;
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ lab2/lab2/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace lab2
+{
+    public partial class MainWindow : Window
+    {
+        CalcArrAsync calcArrAsync1;
+        CalcArrAsync calcArrAsync2;
+        double[] arr;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public enum keys
+        {
+            Sum,
+            Min,
+            Max,
+            SortDesc,
+            SortIncr
+        };
+        void InitArr(int size)
+        {
+            arr = new double[size];
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(-1000, 1000);
+            }
+        }
+        void PercentToPgBar1(int percent)
+        {
+            Dispatcher.Invoke(delegate () {
+                PbTh1.Value = (Convert.ToDouble(percent) / Convert.ToDouble(arr.Length)) * 100;
+                tb1.Text = $"{PbTh1.Value}%/100%";
+            });
+        }
+        void PercentToPgBar2(int percent)
+        {
+            Dispatcher.Invoke(delegate () {
+                PbTh2.Value = (Convert.ToDouble(percent) / Convert.ToDouble(arr.Length)) * 100;
+                tb2.Text = $"{PbTh2.Value}%/100%";
+            });
+        }
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            InitArr(100000);
+            calcArrAsync1 = new CalcArrAsync(arr, keys.Max);
+            calcArrAsync1.PercentProgressHandler = PercentToPgBar1;
+            calcArrAsync1.StartAsync();
+
+            InitArr(10000);
+            calcArrAsync2 = new CalcArrAsync(arr, keys.Min);
+            calcArrAsync2.PercentProgressHandler = PercentToPgBar2;
+            calcArrAsync2.StartAsync();
+        }
+    }
+}
